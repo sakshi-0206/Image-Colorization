8c361dd684e6049933b35890a23d97db68ff1c73
sakshi-0206 <76244677+sakshi-0206@users.noreply.github.com>
2022-06-30 22:02:27 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,48 @@
+import torch
+from models import MainModel
+from utils import lab_to_rgb
+
+def pretrain_generator(net_G, train_dl, opt, criterion, epochs):
+    for e in range(epochs):
+        loss_meter = AverageMeter()
+        for data in tqdm(train_dl):
+            L, ab = data['L'].to(device), data['ab'].to(device)
+            preds = net_G(L)
+            loss = criterion(preds, ab)
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+            
+            loss_meter.update(loss.item(), L.size(0))
+            
+        print(f"Epoch {e + 1}/{epochs}")
+        print(f"L1 Loss: {loss_meter.avg:.5f}")
+
+net_G = build_res_unet(n_input=1, n_output=2, size=256)
+opt = optim.Adam(net_G.parameters(), lr=1e-4)
+criterion = nn.L1Loss()        
+pretrain_generator(net_G, train_dl, opt, criterion, 20)
+torch.save(net_G.state_dict(), "res18-unet.pt")
+
+
+def train_model(model, train_dl, epochs, display_every=200):
+    data = next(iter(val_dl)) 
+    for e in range(epochs):
+        loss_meter_dict = create_loss_meters() 
+        i = 0                                  
+        for data in tqdm(train_dl):
+            model.setup_input(data) 
+            model.optimize()
+            update_losses(model, loss_meter_dict, count=data['L'].size(0))
+            i += 1
+            if i % display_every == 0:
+                print(f"\nEpoch {e+1}/{epochs}")
+                print(f"Iteration {i}/{len(train_dl)}")
+                log_results(loss_meter_dict) 
+                visualize(model, data, save=False) 
+
+net_G = build_res_unet(n_input=1, n_output=2, size=256)
+net_G.load_state_dict(torch.load("res18-unet.pt", map_location=device))
+model = MainModel(net_G=net_G)
+
+train_model(model, train_dl, 50)
